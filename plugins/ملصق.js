e80105087a800fadbae4c078a6cd61111c069256
Mandielhakem <heba.farag.abobakr60@gmail.com>
2024-10-10 19:02:42 +0300

    Update ملصق.js

--- plugins/ملصق.js
+++ plugins/ملصق.js
@@ -4,7 +4,7 @@ import uploadImage from '../lib/uploadImage.js';
 import { webp2mp4 } from '../lib/webp2mp4.js';
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-let stiker = false
+let stiker = mando
 try {
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || q.mediaType || ''
